2b42ba1fb443350b20879faea0a3b5a814002547
Hawariii <ahawarii555@gmail.com>
2026-07-12 00:36:47 +0700

    add config and entry point

---
+++ night vision+/scripts/config.js
@@ -0,0 +1,6 @@
+export const CONFIG = {  
+    interval: 20,  
+    duration: 300,  
+    showParticles: false,  
+    toggleItem: "minecraft:stick"  
+};
---
+++ night vision+/scripts/main.js
@@ -0,0 +1,2 @@
+import "./config.js";
+import "./toggle.js";
